0205c101bf86a84258cde660a7bad11feebc463d
ajdjyt <ajdjking@gmail.com>
2024-10-02 08:06:02 +0530

    hero and product(bugs)

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,7 +1,18 @@
 <script>
     import SectionWrapper from "./SectionWrapper.svelte";
     import Header from "./Header.svelte";
+    import CTAs from "./CTAs.svelte";
 </script>
+
 <SectionWrapper id={"hero"}>
-    <Header />    
+    <Header />
+    <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+        <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold">
+            <span class="text-indigo-400">Choosing</span> Education made easier.
+        </h2>
+        <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] mx-auto w-full">We help you prepare for your future. We are commited to helping you make the right choices for your wants, making sure you know everything you need to.</p>
+        <CTAs/>
+        <div class="flex items-center justify-center gap-2 text-base">
+        </div>
+    </div>
 </SectionWrapper>
--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,7 +1,51 @@
 <script>
     import SectionWrapper from "./SectionWrapper.svelte";
+    import ProductCard from "./ProductCard.svelte";
+
+    let productFeatures = [
+        {   
+            featureName: "CCA",
+            featureList: [     
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque similique nisi earum hic.",
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque similique nisi earum hic.",
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque similique nisi earum hic."
+            ],
+            description:'1.Lorem ipsum, dolor sit amet consectetur adipisicing.',
+            imgUrl: ''
+        },
+        {   
+            featureName: "CCCIS", 
+            featureList: [
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque similique nisi earum hic.",
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque similique nisi earum hic.",
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque similique nisi earum hic."
+            ],
+            description:'2.Lorem ipsum, dolor sit amet consectetur adipisicing.',
+            imgUrl: ''
+        },
+        {   
+            featureName: "CRM",
+            featureList: [
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque similique nisi earum hic.",
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque similique nisi earum hic.",
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus doloremque similique nisi earum hic."
+            ],
+            description:'3.Lorem ipsum, dolor sit amet consectetur adipisicing.',
+            imgUrl: ''
+        }
+    ]; 
+    // ["CCA", "CCCIS", "CRM"]
 </script>
 
-<SectionWrapper id=product>
-    product
+<SectionWrapper id="product">
+    <div class="flex flex-col gap-10 sm:gap-14 mb:gap-24 flex-1 items-center justify-center pb-10 mb:pb-14">
+        <div class="flex flex-col gap-2">
+            <p class="opacity-60 text-base sm:text-lg mb:text-xl text-center">Start getting counselling <span class="text-indigo-400">today.</span></p>
+            <h3 class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center">Our Services</h3>
+        </div>
+        {#each productFeatures as productFeature, index}
+            <ProductCard {productFeature} {index}>
+            </ProductCard>
+        {/each}
+    </div>
 </SectionWrapper>
