6f2060c84cec96ca5525d6da0d2d2f0abf9cecf9
ajdjyt <ajdjking@gmail.com>
2024-10-05 14:53:53 +0530

    add logo and add product buttons

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,19 +1,22 @@
-<script> 
-    let sitename="Aerosol";
+<script>
+	let sitename = 'MentoraGlobal';
 </script>
 
-<header class="flex flex-col relative z-20 "> 
-    <div class="max-w-[1400] mx-auto w-full flex item-center justify-between px-4 py-6">
-        <a href="/">
-            <h1 class="font-semibold">{sitename}</h1>
-        </a>
-        <button class="md:hidden grid place-items-center">
-            <i class="fa-solid fa-bars"></i>
-        </button>
-        <nav class="hidden md:flex items-center gap-4 lg:gap-6">
-            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#product">Product</a>
-            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#aboutus">About Us</a>
-            <button class="specialBtn">Get Started Today</button>
-        </nav>
-    </div>
+<header class="flex flex-col relative z-20">
+	<div class="max-w-[1400em] mx-auto w-full flex item-center justify-between px-4 py-6">
+		<a href="/">
+			<!-- <h1 class="font-semibold">{sitename}</h1> -->
+			<div class="flex flex-col bg-white gap-4 flex-1">
+				<img class="max-w-[7rem]" src="assets/logo.jpeg" alt="" />
+			</div>
+		</a>
+		<button class="md:hidden grid place-items-center">
+			<i class="fa-solid fa-bars"></i>
+		</button>
+		<nav class="hidden md:flex items-center gap-4 lg:gap-6">
+			<a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#product">Product</a>
+			<a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#aboutus">About Us</a>
+			<button class="specialBtn">Get Started Today</button>
+		</nav>
+	</div>
 </header>
