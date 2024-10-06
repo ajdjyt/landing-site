f7f543411de5bde2a9dbc03756185b597a4f1289
ajdjyt <ajdjking@gmail.com>
2024-10-06 14:10:27 +0530

    make header appearance more natural

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -52,16 +52,10 @@
 {/if}
 
 {#if y > outerHeight}
-	<div class="bg-white fixed top-0 left-0 w-full flex flex-col z-20 px-4 fadeIn">
+	<div class="bg-white fixed sticky top-0 left-0 w-full flex flex-col z-20 px-4 fadeIn">
 		<Header />
 	</div>
-	<div style="margin-top:60px;">
-		<slot />
-	</div>
-{:else}
-	<slot />
 {/if}
-
-
+<slot/>
 <Footer />
 <svelte:window bind:scrollY={y} bind:outerHeight />
