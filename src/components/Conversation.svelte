d0f623f3abb96c7e22c8f9a6b4ab1f41777ad04f
ajdjyt <ajdjking@gmail.com>
2024-10-07 17:17:13 +0530

    minor changes

--- src/components/Conversation.svelte
+++ src/components/Conversation.svelte
@@ -7,8 +7,8 @@
 	<div
 		class="py-14 md:py-20 flex flex-col gap-8 bg-[#181b34] text-white text-center items-center justify-center"
 	>
-		<h4 class="text-xl sm:text-2xl md:text-3xl poppins">
-			Get your target institute <span class="poppins font-bold">Today</span>.
+		<h4 class="text-xl sm:text-2xl md:text-3xl">
+			Get your target institute <span class="font-bold">Today</span>.
 		</h4>
 		<p class="poppins text-base sm:text-lg md:text-xl">Tagline</p>
 		<CtAs dark={true} />
--- src/components/FAQs.svelte
+++ src/components/FAQs.svelte
@@ -24,9 +24,9 @@
 				Frequently Asked Questions
 			</h3>
 		</div>
-		<div class="flex flex-col gap-8 sm:gap-10 md:gap-14 w-full">
+		<div class="flex flex-col gap-8 sm:gap-10 md:gap-14 w-full border place-items-center">
 			{#each faqs as faq}
-				<div class="flex flex-col gap-2 text-left max-w-[800px] w-full mx-auto relative p-4 px-6">
+				<div class="flex flex-col gap-2 text-left w-full mx-auto relative p-4 px-6">
 					<div class="absolute top-0 left-0 w-1/3 h-[1px] bg-slate-950 -translate-x-4"></div>
 					<div class="absolute top-0 left-0 h-2/3 w-[1px] bg-slate-950 -translate-y-4"></div>
 					<h4 class="text-lg sm:text-xl md:text-2xl pt-2">
