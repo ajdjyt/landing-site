9b5f55fab421b84259a2312ef63a57543cafe07c
ajdjyt <ajdjking@gmail.com>
2024-10-15 11:29:05 +0530

    remove tagline from conversation

--- src/components/Conversation.svelte
+++ src/components/Conversation.svelte
@@ -10,7 +10,7 @@
 		<h4 class="text-xl sm:text-2xl md:text-3xl">
 			Get your target institute <span class="font-bold">Today</span>.
 		</h4>
-		<p class="poppins text-base sm:text-lg md:text-xl">Tagline</p>
-		<CtAs dark={true} />
+		<!-- <p class="poppins text-base sm:text-lg md:text-xl">Tagline</p> -->
+		<CtAs />
 	</div>
 </section>
